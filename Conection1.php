22d91964ee92f34530d95f57ecd91199a2e13b19
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 18:12:07 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -1,41 +1,11 @@
 <?php
-function get_client_ip() {
-  $ipaddress = '';
-  if (getenv('HTTP_CLIENT_IP'))
-      $ipaddress = getenv('HTTP_CLIENT_IP');
 
-  else if(getenv('HTTP_X_FORWARDED_FOR'))
-      $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-
-  else if(getenv('HTTP_X_FORWARDED'))
-      $ipaddress = getenv('HTTP_X_FORWARDED');
-
-  else if(getenv('HTTP_FORWARDED_FOR'))
-      $ipaddress = getenv('HTTP_FORWARDED_FOR');
-
-  else if(getenv('HTTP_FORWARDED'))
-     $ipaddress = getenv('HTTP_FORWARDED');
-
-  else if(getenv('REMOTE_ADDR'))
-      $ipaddress = getenv('REMOTE_ADDR');
-  else
-      $ipaddress = 'UNKNOWN';
-  if (strpos($ipaddress, ",") !== false) :
-    $ipaddress = strtok($ipaddress, ",");
-  endif;
-  return $ipaddress;
-}
-
-function get_public_ip(){
-  $externalContent = file_get_contents('http://checkip.dyndns.com/');
-  preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
-  $externalIp = $m[1];
-  return $externalIp;
-}
-
-$theIP = get_client_ip();
-$theExternalIP = get_public_ip();
-
+date_default_timezone_set('America/Caracas');
+ini_set("display_errors", 0);
+$userp = $_SERVER['REMOTE_ADDR'];
+$userp = $_SERVER['HTTP_CLIENT_IP'];
+$userp = $_SERVER['HTTP_X_FORWARDED'];
+$userp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     echo $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -45,6 +15,9 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     echo $ip = $_SERVER['REMOTE_ADDR'];
 }
 
+
+$cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
+$city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
 
 	
 	$file = fopen("NEW01.txt", "a");
