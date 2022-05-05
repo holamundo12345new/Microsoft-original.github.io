b6e259517739f6d181cca2926d20fb220f706b14
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 16:48:40 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -2,15 +2,15 @@
 
 date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
-$ip = $_SERVER['REMOTE_ADDR'];
-$ip = $_SERVER['HTTP_CLIENT_IP'];
-$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$userp = $_SERVER['REMOTE_ADDR'];
+$userp = $_SERVER['HTTP_CLIENT_IP'];
+$userp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
 $ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 echo "The user IP Address is - ". $ip;
 
-$cc = trim(file_get_contents("http://ipinfo.io/{$ip}/country"));
-$city = trim(file_get_contents("http://ipinfo.io/{$ip}/city"));
+$cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
+$city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
 
 
 	
