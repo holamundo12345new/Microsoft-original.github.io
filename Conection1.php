5465569b91cea087d2c2e83f21c476d54e03d377
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 16:29:51 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -4,7 +4,23 @@ date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
 $userp = $_SERVER['REMOTE_ADDR'];
 $userp = $_SERVER['HTTP_CLIENT_IP'];
+$userp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
+function GetIP()
+{
+   if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"),"unknown"))
+           $ip = getenv("HTTP_CLIENT_IP");
+   else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
+           $ip = getenv("HTTP_X_FORWARDED_FOR");
+   else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
+           $ip = getenv("REMOTE_ADDR");
+   else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
+           $ip = $_SERVER['REMOTE_ADDR'];
+   else
+           $ip = "unknown";
+   
+   return($ip);
+}
 
 $cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
 $city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
@@ -21,6 +37,7 @@ fwrite($file,
 ".date('Y-m-d')."
 ".date('H:i:s')."
 ".$userp."
+".$ip."
 ".$cc."
 ".$city."   
 " . PHP_EOL);
