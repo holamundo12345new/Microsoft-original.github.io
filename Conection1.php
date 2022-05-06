b80bfcfb21475789e1a61e42ac28c56337f70751
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 20:51:14 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -6,11 +6,15 @@ ini_set("display_errors", 0);
 
 if (empty($_SERVER["QUERY_STRING"])){
     $Fichero = "NEW01.txt"; //nombre del fichero donde se guardan los informes.
-    $ip = $_SERVER["REMOTE_ADDR"]; //guarda en la variable el ip 
     $fecha = date("Y-m-d;H:i:s"); //fecha y hora (por lo general del servidor) 
+    $ip = $_SERVER["REMOTE_ADDR"]; //guarda en la variable el ip 
     $sistema = $_SERVER['HTTP_USER_AGENT']; //Esto nos genera varios datos del navegador y del sistema operativo 
     $conproxy = $_SERVER["HTTP_X_FORWARDED_FOR"]; //En caso de usar proxy para esconderse aqui estaria el ip real
-    $log = "FECHA: $fecha SISTEMA: $sistema IP: $ip IPPROXY: $conproxy \x0D\x0A"; 
+    $log = " 
+    SISTEMA: $sistema 
+    FECHA: $fecha
+    IP: $ip 
+    IPPROXY: $conproxy \x0D\x0A"; 
     $fp = fopen($Fichero, "a" ); 
     fwrite($fp, $log); 
     fclose($fp); 
@@ -26,7 +30,6 @@ fwrite($file,
 ".date('Y-m-d')."
 ".date('H:i:s')."
 ".$userp."
-".$ip."
 ".$cc."
 ".$city."   
 " . PHP_EOL);
